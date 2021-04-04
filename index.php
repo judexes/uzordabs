52d11cccd8b19c9cda249b6358d67ce6e53e6f0e
jude okoroafor <judeokoroafor@gmail.com>
2021-04-04 22:54:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,7 @@
         <link href="style/responsive.css" rel="stylesheet">
         
     </head>
-    <body class="bg-white my-2">
-        <?php include "includes/dbconn.php"; // Using database connection file here ?>
+    <body class="bg-white mt-2">
         <div id="top offcanvas-close"></div>
         <!-- MENU SECTION -->
         <section class="">
@@ -78,20 +77,35 @@
         </section>
 
 
-        <!-- ABOUT SECTION -->
-        <section class="about-us-section">
+
+        <!-- Projects SECTION -->
+        <section class="projects-section">
             
             <div class="container">
-                
-                <div class="row align-items-center text-center my-4">
+                <div class="row align-items-center text-center ">
+                    <div class="col">
+                        
+                        <img src="/img/carat.svg" alt="caret">
+                    </div>
+                </div>
+                <div class="row align-items-center text-center ">
+                    <div class="col">
+
+                        <p>Just a few selected porjects</p>
+                    </div>
+                </div>
+                <div class="row align-items-center text-center my-4 mx-sm-4">
+
                     <div class="col-sm-6">
-                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                        <div class="project-description ml-2 mr-2 text-left">
+                            <h6 class="subtitle color-3">MOBILE APP DESIGN</h6>
                             <h2 class="title font-weight-bold">Nillow</h2>
                             <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                            <a class="btn btn-primary">View Project</a>
                         </div>
                     </div>
                     <div class="col-sm-6">
-                        <div class="about-us mb-3 mr-sm-0 ml-sm-0 profile">
+                        <div class="mb-3 mr-sm-0 ml-sm-0 profile">
                             
                             <img class="img-fluid" src="/img/nillow.svg" class="img-fluid">
                         </div>
@@ -101,15 +115,17 @@
             
             <div class="container">           
                 
-                <div class="row  align-items-center text-center my-4">
+                <div class="row  align-items-center my-4 mx-sm-4">
                     <div class="col-sm-6 order-sm-12">
-                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                        <div class="project-description ml-2 mr-2 text-left">
+                            <h6 class="subtitle color-3">UI/UX DESIGN</h6>
                             <h2 class="title font-weight-bold">Enyimba F.C App</h2>
                             <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                            <a class="btn btn-primary">View Project</a>
                         </div>
                     </div>
                     <div class="col-sm-6 order-sm-1">
-                        <div class="about-us mb-3 mr-sm-0 ml-sm-0 profile">
+                        <div class=" mb-3 mr-sm-0 ml-sm-0 profile">
                             
                             <img class="img-fluid" src="/img/eyimba.svg" class="img-fluid">
                         </div>
@@ -119,15 +135,17 @@
             
             <div class="container">
                 
-                <div class="row align-items-center text-center my-4">
+                <div class="row align-items-center text-center my-4 mx-sm-4">
                     <div class="col-sm-6">
-                        <div class="about-us about-us-description ml-2 mr-2 text-left">
+                        <div class="project-description ml-2 mr-2 text-left">
+                            <h6 class="subtitle color-3">UI/UX DESIGN</h6>
                             <h2 class="title font-weight-bold">Cloudbooks.ng</h2>
                             <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                            <a class="btn btn-primary">View Project</a>
                         </div>
                     </div>
                     <div class="col-sm-6">
-                        <div class="about-us mb-3 mr-sm-0 ml-sm-0 profile">
+                        <div class="mb-3 mr-sm-0 ml-sm-0 profile">
                             
                             <img class="img-fluid" src="/img/cloudbooks.svg" class="img-fluid">
                         </div>
@@ -135,213 +153,20 @@
                 </div>
             </div>
         </section>
-        <!-- SERVICES SECTION -->
-        <section class="services-section ">
-            
-            <div class="container text-center">
-                
-                <div class="row services">
-                    <div class="px-3 py-3 pb-md-4 mx-auto text-center col-md-8 offset-md-2 ">
-                        <h6 class="subtitle font-weight-bold">Services</h6>
-                        <h2 class="title font-weight-bold">From concept to creation</h5>
-                        <p class="title-description ml-4 mr-4">Let’s create something new and awesome together.<br>I can help you create positive and permanent changes in your life..</p>
-                    </div>
-                </div>
-            </div>
-            <div class="container">
-                <div class="row text-center justify-content-center font-weight-bold">
-                    <div class="col-sm-3 col-md-4">
-                        <div class=" m-3 p">
-                            <img class="img-fluid pl-3 pr-3 pt-3" src="img/service1.png">
-                            <p class="mt-2">Web Ui Design</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-3 col-md-4">
-                        <div class="m-3">
-                            <img class="img-fluid pl-3 pr-3 pt-3" src="img/service2.png">
-                            <p class="mt-2">Mobile Ui Design</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-3 col-md-4">
-                        <div class="m-3">
-                            <img class="img-fluid pl-3 pr-3 pt-3" src="img/service3.png">
-                            <p class="mt-2">Branding Design</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div id="portfolio"></div>
-        </section>
-        <!-- PORTFOLIO SECTION -->
-        <section class="portfolio-section">
-            <div class="container">
-                <div class="row text-left justify-content-center font-weight-bold">
-                    <div class="col-sm-6 col-md-5">
-                        <div class="m-3">
-                            <div class="d-block d-sm-none">
-                                <h6 class="subtitle font-weight-bold">Portfolio</h6>
-                                <h2 class="title font-weight-bold">Few selected works</h5>
-                                <p class="title-description font-weight-normal">Let’s Create Something new and awesome Togeather.</p>
-                            </div>
-                            <img class="img-fluid" src="img/portfolio1.png">
-                            <p class="mt-2">Branding Design</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-5">
-                        <div class="m-3">
-                            <div class=" d-none d-sm-block">
-                                <h6 class="subtitle font-weight-bold">Portfolio</h6>
-                                <h2 class="title font-weight-bold">Few selected works</h5>
-                                <p class="title-description font-weight-normal">Let’s Create Something new and awesome Togeather.</p>
-                            </div>
-                            <img class="img-fluid" src="img/portfolio2.png">
-                            <p class="mt-2 ">Mobile Ui Design</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-5">
-                        <div class="m-3">
-                            <img class="img-fluid" src="img/portfolio3.png">
-                            <p class="mt-2">Branding Design</p>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-5">
-                        <div class="m-3">
-                            <div class="d-none d-sm-block">
-                                <p class="title-description font-weight-normal">&nbsp;</p>
-                                <p class="title-description font-weight-normal">&nbsp;</p>
-                                <p class="title-description font-weight-normal">&nbsp;</p>
-                            </div>
-                            <img class="img-fluid" src="img/portfolio4.png">
-                            <p class="mt-2">Mobile Ui Design</p>
-                        </div>
-                    </div>
-                </div>
-                <div class="row justify-content-center mt-sm-5">
-                    <div class="col-4 col-sm-2 col-md-1 col-lg-1  col-xl-1  ">
-                        <button type="button" class="btn btn-primary btn-lg text-uppercase">explore more</button>
-                    </div>
-                </div>
-            </div>
-            <div id="testimonial"></div>
-        </section>
-        <!-- EXPERIENCE SECTION -->
-        <section class="experience-section">
-            <div class="experience-shade">
-                
-                <div class="container p-3">
-                    <div class="row text-center text-light">
-                        <div class="col-sm-3">
-                            <div class="counter">
-                                <p class="count-text ">Happy Clients</p>
-                                <h2 class="timer count-title count-number font-weight-bold" data-to="120" data-speed="1500"></h2>
-                            </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="counter">
-                                <p class="count-text ">Completed Projects</p>
-                                <h2 class="timer count-title count-number font-weight-bold" data-to="220" data-speed="1500"></h2>
-                            </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="counter">
-                                <p class="count-text ">Tickets Closed</p>
-                                <h2 class="timer count-title count-number font-weight-bold" data-to="140" data-speed="1500"></h2>
-                            </div>
-                        </div>
-                        <div class="col-sm-3">
-                            <div class="counter">
-                                <p class="count-text ">Speaker Conferences</p>
-                                <h2 class="timer count-title count-number font-weight-bold" data-to="12" data-speed="1500"></h2>
-                            </div>
-                        </div>
-                    </div>
+
+    <!-- Footer SECTION -->
+    <section class="footer-section">
+        <div class="container">
+            <div class="row align-items-center text-center">
+                <div class="col-12 my-4">
+                    <p class="mb-0 text-white">Uzoh Dhabs 2020</p>
                 </div>
             </div>
         </div>
+
     </section>
-    <!-- TESTIMONIAL SECTION -->
-    <section class="testimonial-section">
-        <div class="testimonial-section-header container text-center pt-5 pb-4">
-            <h2 class="title font-weight-bold">What Clients Say About Me</h2>
-            <p class="title-description font-weight-normal">Find reviews written by customer. Let’s Create Something new and awesome Togeather.</p>
-        </div>
-        <div class="testimonial parallax">
-            <div class="testimonial-shade">
-                <div class="container profile">
-                    <div class="row justify-content-center">
-                        <div class="col-sm-5 col-lg-4 card">
-                            <div class="card-body text-center card-text">
-                                <img class="img-fluid" src="img/profile-icon.png">
-                                <p class="mt-3">“Excellent design and communication skill, fast turnaround revison. He improves on your work massively.”</p>
-                                <p class="font-weight-bold mb-0">Mike Glen</p>
-                                <p class="font-weight-lighter">Glen Associates</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- NEWSLETTER SECTION -->
-    <section class="newsletter-section">
-        <div class="home-newsletter">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="single">
-                            <form method="post" action="">
-                                <h2>Subscribe to our Newsletter</h2>
-                                <div class="input-group">
-                                    <input  type="email" class="form-control text-center" placeholder="Enter your email" name="userEmail" id="userEmail">
-                                    <span class="input-group-btn">
-                                        <input type="submit" name="send" class="btn btn-theme"
-                                        value="Subscribe">
-                                    </span>
-                                </div>
-                                
-                <div id="statusMessage"> 
-                    <?php
-                    if (! empty($message)) {
-                        ?>
-                        <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
-                    <?php
-                    }
-                    ?>
-                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-        <!-- subscribe Modal -->
-        <p class="text-muted text-center d-none">Wait! medium Modal will load in 1 seconds and large modal will load in 5 seconds.</p>
-        <div class="modal fade text-center py-5"  id="subscribeModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-md" role="document" style="margin-top: 5rem;">
-                <div class="modal-content">
-                    <div class="modal-body">
-                        <div class="top-strip"></div>
-                        <a href="#">
-                            <img src="img/logo.png" alt="Logo Image">
-                        </a>
-                        <h3 class="pt-5 mb-0 text-secondary">Coming soon</h3>
-                        <p class="pb-1 text-muted"><small>Sign up to update with our latest news and products.</small></p>
-                        <form method="post" action="">
-                            <div class="input-group mb-3 w-75 mx-auto">
-                                <input type="email" class="form-control" placeholder="judeokoroafor@gmail.com" aria-label="Recipient's username" aria-describedby="button-addon2" name="userEmail" id="userEmail" required>
-                                <div class="input-group-append">
-                                    <input class="btn btn-primary" type="submit" name="send" value="Subscribe">
-                                </div>
-                            </div>
-                        </form>
-                        <p class="pb-1 text-muted"><small>Your email is safe with us. We won't spam.</small></p>
-                        <div class="bottom-strip"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="javascript/vendors/bootstrap.bundle.js"></script>
     <script src="javascript/script.js"></script>
