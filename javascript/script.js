923ffb450187e75b92d8a5306cf06c4b0519b314
jude okoroafor <judeokoroafor@gmail.com>
2021-04-04 03:40:43 +0100

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,29 @@
+
+
+// MOBILE NAVBAR TOGGLE
+$(function () {
+    'use strict'
+  
+    $('[data-toggle="offcanvas"]').on('click', function () {
+      $('.offcanvas-collapse').toggleClass('open');
+      $('.offcanvas-close').toggleClass('close');
+    })
+  });
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("logo").style.width = "30px";
+  } else {
+    document.getElementById("logo").style.width = "50px";
+  }
+}
+
+
+// RELOAD PAGE
+$('#Reloadpage').click(function() {
+    location.reload();
+}); 
