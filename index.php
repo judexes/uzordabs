8cfa327bbc4ad5b8a06898667afa6b6b845156cb
jude okoroafor <judeokoroafor@gmail.com>
2021-04-06 13:44:58 +0100

    Buttons and Dark mode

--- index.php
+++ index.php
@@ -1,131 +1,127 @@
 <!doctype html>
 <html lang="en">
-    <head>
-        
-        <!-- Meta Tags -->
-        <title>Hi I'm Uzoh Daberechi, a Visual & Product Designer.</title>
-        <meta name="title" content="Hi I'm Uzoh Daberechi, a Visual & Product Designer.">
-        <meta name="description" content="Your typical designer's portfolio">
-        <meta name="author" content="Uzoh Daberechi">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-        <meta name="robots" content="index, follow">
-        <meta name="language" content="English">
-        <meta name="keywords" content="Uzoh Daberechi, Visual & Product Designer, Designer">
-        <!-- Bootstrap core CSS -->
-        <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
-        <!-- Darkmode CSS -->
-        <link rel="stylesheet" href="style/dark-mode.css">
-        <!-- Custom styles for this template -->
-        <link href="style/main.css" rel="stylesheet">
-        <!-- Custom styles for this template -->
-        <link href="style/responsive.css" rel="stylesheet">
-        
-    </head>
-    <body class="bg-white mt-2">
-        <div id="top offcanvas-close"></div>
-        <!-- MENU SECTION -->
-        <section class="">
-            <!-- ------------------------ MAIN NAV ----------------------- -->
-            <nav class="navbar navbar-expand-md fixed-top navbar-dark navbar-main bg-white">
-                <a class="navbar-brand mr-auto mr-lg-0 reload-page" href="#" id="Reloadpage" style="min-width: 40px;">
-                    <img src="img/logo.svg" alt="Logo Image" id="logo">
-                </a>
-                <div class="navbar-collapse offcanvas-collapse" id="navbarsExampleDefault">
-                    <ul class="navbar-nav mr-auto text-uppercase mx-auto text-center">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#top">Work</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#about">About me</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#services">Resume</a>
-                        </li>
-                        <span class="align-bottom d-block d-sm-none fixed-bottom mb-5 mode text-center" id="">
-                            <div class="custom-control custom-switch">
-                                <input type="checkbox" class="custom-control-input" id="darkSwitch" />
-                                <label class="custom-control-label" for="darkSwitch"></label>
-                            </div>
-                        </span>
-                    </ul>
-                </div>
-                <span class=" mr-auto mr-lg-0 d-none d-sm-block mode " id="">
-                                    
-                    <div class="custom-control custom-switch">
-                        <input type="checkbox" class="custom-control-input" id="darkSwitch" />
-                        <label class="custom-control-label" for="darkSwitch"></label>
-                    </div>
-                </span>
 
-                <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-                    <img src="img/hamburger.png" alt="hamburger" class="navbar-toggler-icon">
-                    <!-- <span class="navbar-toggler-icon"></span> -->
-                </button>
-            </nav>
-            <!----------------------- SECONDARY NAV ------------------------->
-        </section>
+<head>
 
+    <!-- Meta Tags -->
+    <title>Hi I'm Uzoh Daberechi, a Visual & Product Designer.</title>
+    <meta name="title" content="Hi I'm Uzoh Daberechi, a Visual & Product Designer.">
+    <meta name="description" content="Your typical designer's portfolio">
+    <meta name="author" content="Uzoh Daberechi">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="keywords" content="Uzoh Daberechi, Visual & Product Designer, Designer">
+    <!-- Bootstrap core CSS -->
+    <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style/main.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style/responsive.css" rel="stylesheet">
+    <!-- Darkmode CSS -->
+    <link rel="stylesheet" href="style/dark-mode.css">
 
-        <!-- HERO SECTION -->
-        <section class="hero-section">
-            <div class="splash">
-                <div class="container">
-                    <div class="row align-items-center mx-2">
-                        <div class="col-sm-7 align-middle px-0 px-sm-2 text-left">
-                            <h1 class="display-4 font-weight-bolder color-1">Hi I'm Uzoh Daberechi, a</h1>
-                            <h1 class="display-4 font-weight-bolder color-2">Visual & Product Designer.</h1>
-                            <p class="">I love to design experiences and usable digital products loved by people.</p>
-                        </div>
-                        <div class="col-sm-5 my-4">
-                            <img src="img/profile.svg" alt="photo" class="img-fluid hero-graphic-img">
-                        </div>
-                    </div>
-                </div>
+</head>
+
+<body class="bg-white mt-2">
+    <div id="top offcanvas-close"></div>
+    <!-- MENU SECTION -->
+    <section class="">
+        <!-- ------------------------ MAIN NAV ----------------------- -->
+        <nav class="navbar navbar-expand-md fixed-top navbar-dark navbar-main bg-white">
+            <a class="navbar-brand mr-auto mr-lg-0 reload-page" href="#" id="Reloadpage" style="min-width: 40px;">
+                <img src="img/logo.svg" alt="Logo Image" id="logo">
+            </a>
+            <div class="navbar-collapse offcanvas-collapse bg-white" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-auto mx-auto text-center">
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">Work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">About me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">Resume</a>
+                    </li>
+                </ul>
             </div>
-        </section>
+            <span class=" mr-auto mr-lg-0 d-none d-sm-block mode " id="">
+
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+                    <label class="custom-control-label" for="darkSwitch"></label>
+                </div>
+            </span>
+
+            <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+                <img src="img/hamburger.png" alt="hamburger" class="navbar-toggler-icon">
+                <!-- <span class="navbar-toggler-icon"></span> -->
+            </button>
+        </nav>
+        <!----------------------- SECONDARY NAV ------------------------->
+    </section>
 
 
-
-        <!-- Projects SECTION -->
-        <section class="projects-section">
-            
+    <!-- HERO SECTION -->
+    <section class="hero-section">
+        <div class="splash">
             <div class="container">
-                <div class="row align-items-center text-center ">
-                    <div class="col">
-                        
-                        <img src="img/carat.svg" alt="caret">
+                <div class="row align-items-center mx-2">
+                    <div class="col-sm-7 align-middle px-0 px-sm-2 text-left">
+                        <h1 class="display-4 font-weight-bolder color-1">Hi I'm Uzoh Daberechi, a</h1>
+                        <h1 class="display-4 font-weight-bolder color-2">Visual & Product Designer.</h1>
+                        <p class="">I love to design experiences and usable digital products loved by people.</p>
                     </div>
-                </div>
-                <div class="row align-items-center text-center ">
-                    <div class="col">
-
-                        <p>Just a few selected porjects</p>
-                    </div>
-                </div>
-                <div class="row align-items-center text-center my-4 mx-sm-4">
-
-                    <div class="col-sm-6">
-                        <div class="project-description ml-2 mr-2 text-left">
-                            <h6 class="subtitle color-3">MOBILE APP DESIGN</h6>
-                            <h2 class="title font-weight-bold">Nillow</h2>
-                            <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
-                            <a class="btn btn-primary">View Project</a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="mb-3 mr-sm-0 ml-sm-0 profile">
-                            
-                            <img class="img-fluid" src="img/nillow.svg" class="img-fluid">
-                        </div>
+                    <div class="col-sm-5 my-4">
+                        <img src="img/profile.svg" alt="photo" class="img-fluid hero-graphic-img">
                     </div>
                 </div>
             </div>
-            
-            <div class="container">           
-                
+        </div>
+    </section>
+
+
+
+    <!-- Projects SECTION -->
+    <section class="projects-section">
+
+        <div class="container">
+            <div class="row align-items-center text-center ">
+                <div class="col">
+
+                    <img src="img/carat.svg" alt="caret">
+                </div>
+            </div>
+            <div class="row align-items-center text-center ">
+                <div class="col">
+
+                    <p>Just a few selected porjects</p>
+                </div>
+            </div>
+
+            <div class="container">
+
+                <div class="row align-items-center  my-4 mx-sm-4">
+                    <div class="col-sm-6 order-sm-12 mt-3 mb-2">
+                        <img class="img-fluid" src="img/nillow.svg" class="img-fluid">
+                    </div>
+                    <div class="col-sm-6 order-sm-1">
+                        <h6 class="subtitle color-3">MOBILE APP DESIGN</h6>
+                        <h2 class="title font-weight-bold">Nillow</h2>
+                        <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                        <a class="btn btn-primary">View Project</a>
+                    </div>
+                </div>
+
                 <div class="row  align-items-center my-4 mx-sm-4">
-                    <div class="col-sm-6 order-sm-12">
+                    <div class="col-sm-6 mt-3 mb-2">
+                        <div class=" mb-3 mr-sm-0 ml-sm-0 profile">
+
+                            <img class="img-fluid" src="img/eyimba.svg" class="img-fluid">
+                        </div>
+                    </div>
+                    <div class="col-sm-6 ">
                         <div class="project-description ml-2 mr-2 text-left">
                             <h6 class="subtitle color-3">UI/UX DESIGN</h6>
                             <h2 class="title font-weight-bold">Enyimba F.C App</h2>
@@ -133,35 +129,22 @@
                             <a class="btn btn-primary">View Project</a>
                         </div>
                     </div>
+                </div>
+
+                <div class="row align-items-center  my-4 mx-sm-4">
+                    <div class="col-sm-6 order-sm-12 mt-3 mb-2">
+                        <img class="img-fluid" src="img/cloudbooks.svg" class="img-fluid">
+                    </div>
                     <div class="col-sm-6 order-sm-1">
-                        <div class=" mb-3 mr-sm-0 ml-sm-0 profile">
-                            
-                            <img class="img-fluid" src="img/eyimba.svg" class="img-fluid">
-                        </div>
+                        <h6 class="subtitle color-3">UI/UX DESIGN</h6>
+                        <h2 class="title font-weight-bold">Cloudbooks.ng</h2>
+                        <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                        <a class="btn btn-primary">View Project</a>
                     </div>
                 </div>
+
             </div>
-            
-            <div class="container">
-                
-                <div class="row align-items-center text-center my-4 mx-sm-4">
-                    <div class="col-sm-6">
-                        <div class="project-description ml-2 mr-2 text-left">
-                            <h6 class="subtitle color-3">UI/UX DESIGN</h6>
-                            <h2 class="title font-weight-bold">Cloudbooks.ng</h2>
-                            <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
-                            <a class="btn btn-primary">View Project</a>
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="mb-3 mr-sm-0 ml-sm-0 profile">
-                            
-                            <img class="img-fluid" src="img/cloudbooks.svg" class="img-fluid">
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+    </section>
 
     <!-- Footer SECTION -->
     <section class="footer-section">
@@ -180,4 +163,5 @@
     <script src="javascript/dark-mode-switch.min.js"></script>
     <script src="javascript/script.js"></script>
 </body>
+
 </html>
