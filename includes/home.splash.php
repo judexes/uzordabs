c4fb1f39eddbca76dacdda9a4300045ce0361bf5
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 14:59:56 +0100

    Create home.splash.php

---
+++ includes/home.splash.php
@@ -0,0 +1,26 @@
+<section class="hero-section">
+    <div class="splash">
+        <div class="container">
+            <div class="row align-items-center mx-2">
+                <div class="align-middle col-sm-7 px-2 px-sm-2 text-sm-left">
+                    <h1 class="display-4 color-1">Hi I'm Uzoh Daberechi, a</h1>
+                    <h1 class="display-4 color-2">Visual & Product Designer.</h1>
+                    <p class="">I love to design experiences and usable digital products loved by people.</p>
+                </div>
+                <div class="col-sm-5 my-4 text-sm-right">
+                    <img src="img/profile.svg" alt="photo" class="img-fluid hero-graphic-img">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row align-items-center text-center ">
+        <div class="col">
+            <a href="#more">
+
+                <img src="img/carat-white.svg" alt="caret" id="carat" class="animate__animated animate__bounce animate__slow 2s animate__infinite infinite">
+            </a>
+        </div>
+    </div>
+</section>
+
+<div id="more" class=""></div>
