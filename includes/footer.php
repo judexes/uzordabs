eba2ab837cb631b466cfac72790c3b1e219e5a85
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 15:00:17 +0100

    Create footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<section class="footer-section">
+    <div class="container">
+        <div class="row align-items-center text-center">
+            <div class="col-12 my-4">
+                <p class="mb-0 text-white">Designed by Dhabs | Developed by Jude</p>
+            </div>
+        </div>
+    </div>
+
+</section>
