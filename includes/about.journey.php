91d90e3aad10fae499e5e15ea3e7e1a96bab11cb
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 17:04:55 +0100

    Update about.journey.php

--- includes/about.journey.php
+++ includes/about.journey.php
@@ -2,37 +2,60 @@
 
     <div class="container">
         <div class="row align-items-center text-center ">
-            <div class="col mt-4">
+            <div class="col mt-0">
 
-                <table class="table">
-                    <caption>List of users</caption>
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">First</th>
-                            <th scope="col">Last</th>
-                            <th scope="col">Handle</th>
-                        </tr>
-                    </thead>
+                <table class="table text-right">
                     <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>@fat</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>@twitter</td>
-                        </tr>
+                        <div class="job">                            
+                            <tr>
+                                <th class="pb-0  text-left">Product Designer</th>
+                                <td class=" pb-1 ">Jan. 2021 - Present</td>
+                            </tr>
+                            <tr class="text-muted">
+                                <td class="text-left border-0  pt-0 pb-3">Gotahia</td>
+                                <td class="border-0   pt-0 pb-3">Lagos, Nigeria</td>
+                            </tr>
+                        </div>
+                        <div class="job">
+                            <tr>
+                                <th class="pb-0  text-left">Creative Designer</th>
+                                <td class=" pb-1 ">Jan. 2021 - Present</td>
+                            </tr>
+                            <tr class="text-muted">
+                                <td class="text-left border-0  pt-0 pb-3">JIggle</td>
+                                <td class="border-0   pt-0 pb-3">Akwa Ibom, Nigeria</td>
+                            </tr>
+                        </div>
+                        <div class="job">
+                            <tr>
+                                <th class="pb-0  text-left">Creative Designer</th>
+                                <td class=" pb-1 ">Jan. 2017 - Dec. 2020</td>
+                            </tr>
+                            <tr class="text-muted">
+                                <td class="text-left border-0  pt-0 pb-3">Innovation Growth Hub</td>
+                                <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
+                            </tr>
+                        </div>
+                        <div class="job">
+                            <tr>
+                                <th class="pb-0  text-left">Contract UI/UX Designer</th>
+                                <td class=" pb-1 ">Oct. 2019 - Mar. 2020</td>
+                            </tr>
+                            <tr class="text-muted">
+                                <td class="text-left border-0  pt-0 pb-3">Enyimba Football Club, Aba</td>
+                                <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
+                            </tr>
+                        </div>
+                        <div class="job">
+                            <tr>
+                                <th class="pb-0  text-left">Graphics Designer</th>
+                                <td class=" pb-1 ">Jan. 2016 - Dec. 2017</td>
+                            </tr>
+                            <tr class="text-muted">
+                                <td class="text-left border-0  pt-0 pb-3">Capacity Development Int’l</td>
+                                <td class="border-0  pt-0 pb-3">Umuahia, Nigeria</td>
+                            </tr>
+                        </div>
                     </tbody>
                 </table>
 
