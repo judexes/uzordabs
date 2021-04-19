175b584e8d47a27fa4fc710274aea6608836e119
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 15:01:06 +0100

    Create about.php

---
+++ about.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+    <!-- Meta Tags -->
+    <title>Hi, I'm Uzoh Daberechi</title>
+    <meta name="title" content="A Visual & Product Designer.">
+    <meta name="description" content="A Visual & Product Designer.">
+    <meta name="author" content="Uzoh Daberechi">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="robots" content="index, follow">
+    <meta name="language" content="English">
+    <meta name="keywords" content="Uzoh Daberechi, Visual & Product Designer, Designer">
+    
+    <!-- Animate CSS -->
+    <link rel="stylesheet" href="style/vendors/animate.css/4.0.0/animate.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="style/vendors/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style/main.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style/responsive.css" rel="stylesheet">
+    <!-- Darkmode CSS -->
+    <link rel="stylesheet" href="style/dark-mode.css">
+
+</head>
+
+<body class="bg-white mt-2">
+
+    <!-- MENU SECTION -->
+    <?php    include_once 'includes/menu.php'    ?>
+    
+    <!-- SPLASH SECTION -->
+    <?php    include_once 'includes/about.splash.php'    ?>
+
+    <!-- Projects SECTION -->
+    <?php    include_once 'includes/about.journey.php'    ?>
+
+    <!-- Footer SECTION -->
+    <?php    include_once 'includes/footer.php'    ?>
+
+    <!-- JS imports -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="javascript/vendors/bootstrap.bundle.js"></script>
+    <script src="javascript/dark-mode-switch.min.js"></script>
+    <script src="javascript/script.js"></script>
+</body>
+
+</html>
