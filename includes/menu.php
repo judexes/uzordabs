aac8dce220bc25db9ec90789a9f5c513199403f5
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 14:59:35 +0100

    Create menu.php

---
+++ includes/menu.php
@@ -0,0 +1,47 @@
+<div id="top offcanvas-close"></div>
+
+<section class="">
+
+    <div class="">
+        <!-- ------------------------ MAIN NAV ----------------------- -->
+        <nav class="navbar navbar-expand-md fixed-top navbar-dark navbar-main bg-white container px-sm-4">
+            <a class="navbar-brand mr-auto mr-lg-0 reload-page" id="Reloadpage" style="min-width: 40px;">
+                <img src="img/logo.svg" alt="Logo Image" id="logo">
+            </a>
+            <div class="navbar-collapse offcanvas-collapse bg-white" id="navbarsExampleDefault">
+                <ul class="navbar-nav mr-sm-5 mx-auto text-center">
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">Work</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">About me</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link  text-black">Resume</a>
+                    </li>
+
+                    <span class="align-bottom d-block d-sm-none fixed-bottom mb-5 socials text-center" id="">
+
+                        <div class="custom-control custom-switch">
+                            <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+                            <label class="custom-control-label" for="darkSwitch"></label>
+                        </div>
+                    </span>
+                </ul>
+            </div>
+            <span class=" mr-auto mr-lg-0 d-none d-sm-block mode " id="">
+
+                <div class="custom-control custom-switch">
+                    <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+                    <label class="custom-control-label" for="darkSwitch"></label>
+                </div>
+            </span>
+
+            <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+                <img src="img/hamburger.png" alt="hamburger" class="navbar-toggler-icon">
+                <!-- <span class="navbar-toggler-icon"></span> -->
+            </button>
+        </nav>
+
+    </div>
+</section>
