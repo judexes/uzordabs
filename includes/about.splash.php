e9c494abe0fc98da999bb0372e686763d8506277
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 15:00:33 +0100

    Create about.splash.php

---
+++ includes/about.splash.php
@@ -0,0 +1,27 @@
+<section class="hero-section about">
+    <div class="splash">
+        <div class="container">
+            <div class="row align-items-center mx-2 mt-sm-4">
+                <div class="align-middle col-sm-7 px-2 px-sm-2 text-sm-left">
+                    <h1 class="display-4 mb-sm-4">A bit about myself.</h1>
+                    <p class="">My name is Uzoh Daberechi and I’m a Visual and Digital Product Designer, I'm passionate about improving the lives of others through design.</p>
+                    <p class="">I'm a self taught designer and have learnt a lot of the processes, concepts and theories of UX/UI and product design through workshops, reading blogs and applying them to projects i have done. Having an experience in Front-end development coupled with UI/UX Design has given me valuable industry exposure to learning and working with both designers and engineers. I'm constantly learning new things everyday and I love meeting people who are motivated and not afraid of sharing their ideas with the world.</p>
+                    <p class="">Currently, I work with <span class="color-2"> Gotahia</span> as the product designer and also as a part-time creative designer for <span class="color-2">Jiggle.ng</span>. I have interest in Web development, motion graphics, photography and animation. I am currently based in Abuja, Nigeria and i enjoy travelling too! </p>
+                </div>
+                <div class="col-sm-5 my-4 text-sm-right">
+                    <img src="img/dp.svg" alt="photo" class="img-fluid hero-graphic-img">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row align-items-center text-center ">
+        <div class="col">
+            <a href="#more">
+
+                <img src="img/carat-white.svg" alt="caret" id="carat" class="animate__animated animate__bounce animate__slow 2s animate__infinite infinite">
+            </a>
+        </div>
+    </div>
+</section>
+
+<div id="more" class=""></div>
