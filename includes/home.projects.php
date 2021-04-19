4834d9da7b34c2df6eae8cdc2add8cb9d0c389e8
jude okoroafor <judeokoroafor@gmail.com>
2021-04-19 15:00:02 +0100

    Create home.projects.php

---
+++ includes/home.projects.php
@@ -0,0 +1,56 @@
+<section class="projects-section">
+
+    <div class="container">
+        <div class="row align-items-center text-center ">
+            <div class="col mt-4">
+
+                <p>Just a few selected porjects</p>
+            </div>
+        </div>
+
+        <div class="container">
+
+            <div class="row align-items-center  my-4">
+                <div class="col-sm-6 order-sm-12 mt-3 mb-2 text-right">
+                    <img class="img-fluid" src="img/nillow.svg" class="img-fluid" id="nillow">
+                </div>
+                <div class="col-sm-6 order-sm-1">
+                    <h6 class="subtitle color-3">MOBILE APP DESIGN</h6>
+                    <h2 class="title font-weight-bold">Nillow</h2>
+                    <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                    <a class="btn btn-primary">View Project</a>
+                </div>
+            </div>
+
+            <div class="row  align-items-center my-4 ">
+                <div class="col-sm-6 mt-3 mb-2">
+                    <div class=" mb-3 mr-sm-0 ml-sm-0">
+
+                        <img class="img-fluid" src="img/eyimba.svg" class="img-fluid" id="eyimba">
+                    </div>
+                </div>
+                <div class="col-sm-6 ">
+                    <div class="project-description ml-2 mr-2 text-left">
+                        <h6 class="subtitle color-3">UI/UX DESIGN</h6>
+                        <h2 class="title font-weight-bold">Enyimba F.C App</h2>
+                        <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                        <a class="btn btn-primary">View Project</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row align-items-center  my-4 ">
+                <div class="col-sm-6 order-sm-12 mt-3 mb-2 text-right">
+                    <img class="img-fluid" src="img/cloudbooks.svg" class="img-fluid" id="cloudbooks">
+                </div>
+                <div class="col-sm-6 order-sm-1">
+                    <h6 class="subtitle color-3">UI/UX DESIGN</h6>
+                    <h2 class="title font-weight-bold">Cloudbooks.ng</h2>
+                    <p class="title-description">A café application that creates a unique reservation experience on the customer's mobile phone.</p>
+                    <a class="btn btn-primary">View Project</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
