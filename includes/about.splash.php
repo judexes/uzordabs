e81cce0a4bd65c7426074dd69f706c49a4a080ce
jude okoroafor <judeokoroafor@gmail.com>
2021-04-21 14:01:39 +0100

    Update about.splash.php

--- includes/about.splash.php
+++ includes/about.splash.php
@@ -2,7 +2,7 @@
     <div class="splash">
         <div class="container">
             <div class="row align-items-center mx-2 mt-sm-4">
-                <div class="align-middle col-sm-7 px-2 px-sm-2 text-sm-left">
+                <div class="align-middle col-sm-7 px-2 px-sm-2 text-left">
                     <h1 class="display-4 mb-sm-4">A bit about myself.</h1>
                     <p class="">My name is Uzoh Daberechi and I’m a Visual and Digital Product Designer, I'm passionate about improving the lives of others through design.</p>
                     <p class="">I'm a self taught designer and have learnt a lot of the processes, concepts and theories of UX/UI and product design through workshops, reading blogs and applying them to projects i have done. Having an experience in Front-end development coupled with UI/UX Design has given me valuable industry exposure to learning and working with both designers and engineers. I'm constantly learning new things everyday and I love meeting people who are motivated and not afraid of sharing their ideas with the world.</p>
