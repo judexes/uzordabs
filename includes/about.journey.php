543d95d93862b7748a86697b51ca650fb6918a07
jude okoroafor <judeokoroafor@gmail.com>
2021-04-21 01:50:57 +0100

    Table alignment fixed

--- includes/about.journey.php
+++ includes/about.journey.php
@@ -2,53 +2,58 @@
 
     <div class="container">
         <div class="row align-items-center text-center ">
-            <div class="col mt-0">
+            <div class="container">
+                <h6 class="font-weight-bolder">My Journey</h6>
+                <div class="row m-0">
+                    <div class="col mt-0">
 
-                <table class="table text-right">
-                    <tbody>
-                        <tr>
-                            <th class="border-0 pb-0  text-left">Product Designer</th>
-                            <td class="border-0 pb-1 ">Jan. 2021 - Present</td>
-                        </tr>
-                        <tr class="text-muted">
-                            <td class="text-left border-0  pt-0 pb-3">Gotahia</td>
-                            <td class="border-0   pt-0 pb-3">Lagos, Nigeria</td>
-                        </tr>
-                        <tr>
-                            <th class="pb-0  text-left">Creative Designer</th>
-                            <td class=" pb-1 ">Jan. 2021 - Present</td>
-                        </tr>
-                        <tr class="text-muted">
-                            <td class="text-left border-0  pt-0 pb-3">JIggle</td>
-                            <td class="border-0   pt-0 pb-3">Akwa Ibom, Nigeria</td>
-                        </tr>
-                        <tr>
-                            <th class="pb-0  text-left">Creative Designer</th>
-                            <td class=" pb-1 ">Jan. 2017 - Dec. 2020</td>
-                        </tr>
-                        <tr class="text-muted">
-                            <td class="text-left border-0  pt-0 pb-3">Innovation Growth Hub</td>
-                            <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
-                        </tr>
-                        <tr>
-                            <th class="pb-0  text-left">Contract UI/UX Designer</th>
-                            <td class=" pb-1 ">Oct. 2019 - Mar. 2020</td>
-                        </tr>
-                        <tr class="text-muted">
-                            <td class="text-left border-0  pt-0 pb-3">Enyimba Football Club, Aba</td>
-                            <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
-                        </tr>
-                        <tr>
-                            <th class="pb-0  text-left">Graphics Designer</th>
-                            <td class=" pb-1 ">Jan. 2016 - Dec. 2017</td>
-                        </tr>
-                        <tr class="border-bottom text-muted">
-                            <td class="text-left border-0  pt-0 pb-3">Capacity Development Int’l</td>
-                            <td class="border-0  pt-0 pb-3">Umuahia, Nigeria</td>
-                        </tr>
-                    </tbody>
-                </table>
+                        <table class="table text-right">
+                            <tbody>
+                                <tr>
+                                    <th class="border-0 pb-0  text-left">Product Designer</th>
+                                    <td class="border-0 pb-1 ">Jan. 2021 - Present</td>
+                                </tr>
+                                <tr class="text-muted">
+                                    <td class="text-left border-0  pt-0 pb-3">Gotahia</td>
+                                    <td class="border-0   pt-0 pb-3">Lagos, Nigeria</td>
+                                </tr>
+                                <tr>
+                                    <th class="pb-0  text-left">Creative Designer</th>
+                                    <td class=" pb-1 ">Jan. 2021 - Present</td>
+                                </tr>
+                                <tr class="text-muted">
+                                    <td class="text-left border-0  pt-0 pb-3">JIggle</td>
+                                    <td class="border-0   pt-0 pb-3">Akwa Ibom, Nigeria</td>
+                                </tr>
+                                <tr>
+                                    <th class="pb-0  text-left">Creative Designer</th>
+                                    <td class=" pb-1 ">Jan. 2017 - Dec. 2020</td>
+                                </tr>
+                                <tr class="text-muted">
+                                    <td class="text-left border-0  pt-0 pb-3">Innovation Growth Hub</td>
+                                    <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
+                                </tr>
+                                <tr>
+                                    <th class="pb-0  text-left">Contract UI/UX Designer</th>
+                                    <td class=" pb-1 ">Oct. 2019 - Mar. 2020</td>
+                                </tr>
+                                <tr class="text-muted">
+                                    <td class="text-left border-0  pt-0 pb-3">Enyimba Football Club, Aba</td>
+                                    <td class="border-0  pt-0 pb-3">Aba, Nigeria</td>
+                                </tr>
+                                <tr>
+                                    <th class="pb-0  text-left">Graphics Designer</th>
+                                    <td class=" pb-1 ">Jan. 2016 - Dec. 2017</td>
+                                </tr>
+                                <tr class="border-bottom text-muted">
+                                    <td class="text-left border-0  pt-0 pb-3">Capacity Development Int’l</td>
+                                    <td class="border-0  pt-0 pb-3">Umuahia, Nigeria</td>
+                                </tr>
+                            </tbody>
+                        </table>
 
+                    </div>
+                </div>
             </div>
         </div>
 
