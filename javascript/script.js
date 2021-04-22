2828e2427cec21034994af14eff5f0153824fa75
jude okoroafor <judeokoroafor@gmail.com>
2021-04-22 22:57:32 +0100

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,18 +1,17 @@
-
-
 // MOBILE NAVBAR TOGGLE
 $(function () {
-    'use strict'
-  
-    $('[data-toggle="offcanvas"]').on('click', function () {
-      $('.offcanvas-collapse').toggleClass('open');
-      $('.offcanvas-close').toggleClass('close');
-    })
-  });
+  "use strict";
 
+  $('[data-toggle="offcanvas"]').on("click", function () {
+    $(".offcanvas-collapse").toggleClass("open");
+    $(".offcanvas-close").toggleClass("close");
+  });
+});
 
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -22,70 +21,15 @@ function scrollFunction() {
   }
 }
 
-
 // RELOAD PAGE
-$('#Reloadpage').click(function() {
-    location.reload();
-}); 
-
-
-// MOBILE MENU
-	
-if( 'ontouchstart' in window ){ var click = 'touchstart'; }
-else { var click = 'click'; }
-
-
-$('div.burger').on(click, function(){
-
-    if( !$(this).hasClass('open') ){ openMenu(); } 
-    else { closeMenu(); }
-
+$("#Reloadpage").click(function () {
+  location.reload();
 });
 
-
-$('div.menu ul li a').on(click, function(e){
-  e.preventDefault();
-  closeMenu();
-});		
-
-
-function openMenu(){
-  
-  $('div.circle').addClass('expand');
-        
-  $('div.burger').addClass('open');	
-  $('div.x, div.y, div.z').addClass('collapsse');
-  $('.menu li').addClass('animate');
-  
-  setTimeout(function(){ 
-    $('div.y').hide(); 
-    $('div.x').addClass('rotate30'); 
-    $('div.z').addClass('rotate150'); 
-  }, 70);
-  setTimeout(function(){
-    $('div.x').addClass('rotate45'); 
-    $('div.z').addClass('rotate135');  
-  }, 120);
-  
-  
-
-}
-
-function closeMenu(){
-
-  $('div.burger').removeClass('open');	
-  $('div.x').removeClass('rotate45').addClass('rotate30'); 
-  $('div.z').removeClass('rotate135').addClass('rotate150');				
-  $('div.circle').removeClass('expand');
-  $('.menu li').removeClass('animate');
-  
-  setTimeout(function(){ 			
-    $('div.x').removeClass('rotate30'); 
-    $('div.z').removeClass('rotate150'); 			
-  }, 50);
-  setTimeout(function(){
-    $('div.y').show(); 
-    $('div.x, div.y, div.z').removeClass('collapsse');
-  }, 70);													
-  
-}
+// MOBILE MENU
+$(".menu-burger, .menu-items").on("click", function () {
+  $(".menu-bg, .menu-items, .menu-burger").toggleClass("fs");
+  $(".menu-burger").text() == "☰"
+    ? $(".menu-burger").text("✕")
+    : $(".menu-burger").text("☰");
+});
