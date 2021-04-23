5f3acf942e7d6e65b3ac1d4acc3acded535af6bb
jude okoroafor <judeokoroafor@gmail.com>
2021-04-23 15:25:09 +0100

    Update to mobile menu

--- includes/menu.php
+++ includes/menu.php
@@ -44,20 +44,21 @@
         </nav>
 
         <!-- MOBILE NAV -->
-        <div class="d-block d-sm-none " id="mob-menu">
-		
+        <div class="d-block d-sm-none container">
+            <div class="mobile-menu row">
 
-            <div class="menu-bg"></div>
-            <div class="menu-logo" >
-                <img src="/img/logo.svg" alt="logo" class="img-fluid ">
-            </div>
-            <div class="menu-burger p-1">☰</div>
-            <!-- <div class="menu-burger p-1">☰</div> -->
-            <div class="menu-items">
-                <div><a href="/">Work</a></div>
-                <div><a href="about">About me</a></div>
-                <div><a href="/Principles-by-Ray-Dalio.pdf">Resume</a></div>
-                <div><a href="">Darkmode</a></div>
+                <div class="menu-bg"></div>
+                <div class="menu-logo" >
+                    <img src="img/logo.svg" alt="logo" class="img-fluid ">
+                </div>
+                <div class="menu-burger p-1">☰</div>
+                <!-- <div class="menu-burger p-1">☰</div> -->
+                <div class="menu-items">
+                    <div><a href="/">Work</a></div>
+                    <div><a href="about">About me</a></div>
+                    <div><a href="/Principles-by-Ray-Dalio.pdf">Resume</a></div>
+                    <div><a href="">Darkmode</a></div>
+                </div>
             </div>
 
         </div>
