9f06d725b580ae603d78901505bd177f8fdd33dd
jude okoroafor <judeokoroafor@gmail.com>
2021-04-24 04:20:59 +0100

    Large screen menu fix

--- includes/menu.php
+++ includes/menu.php
@@ -4,51 +4,51 @@
 
     <div class="">
         <!-- MAIN NAV   -->
-        <nav class="navbar navbar-expand-md fixed-top navbar-dark navbar-main bg-white container d-none d-sm-block">
-            <a class="navbar-brand mr-auto mr-lg-0 reload-page" id="Reloadpage" style="min-width: 40px;">
-                <img src="img/logo.svg" alt="Logo Image" id="logo">
-            </a>
-            <div class="navbar-collapse offcanvas-collapse bg-white" id="navbarsExampleDefault">
-                <ul class="navbar-nav mr-sm-5 mx-auto text-center">
-                    <li class="nav-item">
-                        <a class="nav-link text-black" href="/">Work</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-black" href="about">About me</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link text-black" href="/Principles-by-Ray-Dalio.pdf" download="Principles-by-Ray-Dalio.pdf" target="_blank">Resume</a>
-                    </li>
-
-                    <span class="align-bottom d-block d-sm-none fixed-bottom mb-5 socials text-center" id="">
-
-                        <div class="custom-control custom-switch">
-                            <input type="checkbox" class="custom-control-input" id="darkSwitch" />
-                            <label class="custom-control-label" for="darkSwitch"></label>
-                        </div>
-                    </span>
-                </ul>
-            </div>
-            <span class=" mr-auto mr-lg-0 d-none d-sm-block mode " id="">
-
-                <div class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="darkSwitch" />
-                    <label class="custom-control-label" for="darkSwitch"></label>
+        <div class=" d-none d-sm-block">
+            <!-- ------------------------ MAIN NAV ----------------------- -->
+            <nav class="navbar navbar-expand-md fixed-top navbar-dark navbar-main bg-white container px-sm-4">
+                <a class="navbar-brand mr-auto mr-lg-0 reload-page" id="Reloadpage" style="min-width: 40px;">
+                    <img src="img/logo.svg" alt="Logo Image" id="logo">
+                </a>
+                <div class="navbar-collapse offcanvas-collapse bg-white" id="navbarsExampleDefault">
+                    <ul class="navbar-nav mr-sm-5 mx-auto text-center">
+                        <li class="nav-item">
+                            <a class="nav-link  text-black">Work</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  text-black">About me</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link  text-black">Resume</a>
+                        </li>
+                        <!-- <span class="align-bottom d-block d-sm-none fixed-bottom mb-5 socials text-center" id="">
+                            <div class="custom-control custom-switch">
+                                <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+                                <label class="custom-control-label" for="darkSwitch"></label>
+                            </div>
+                        </span> -->
+                    </ul>
                 </div>
-            </span>
+                <span class=" mr-auto mr-lg-0 d-none d-sm-block mode " id="">
+                    <div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id="darkSwitch" />
+                        <label class="custom-control-label" for="darkSwitch"></label>
+                    </div>
+                </span>
+                <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
+                    <img src="img/hamburger.png" alt="hamburger" class="navbar-toggler-icon">
+                    <!-- <span class="navbar-toggler-icon"></span> -->
+                </button>
+            </nav>
 
-            <button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas">
-                <img src="img/hamburger.png" alt="hamburger" class="navbar-toggler-icon">
-                <!-- <span class="navbar-toggler-icon"></span> -->
-            </button>
-        </nav>
+        </div>
 
         <!-- MOBILE NAV -->
         <div class="d-block d-sm-none container">
             <div class="mobile-menu row">
 
                 <div class="menu-bg"></div>
-                <div class="menu-logo" >
+                <div class="menu-logo">
                     <img src="img/logo.svg" alt="logo" class="img-fluid ">
                 </div>
                 <div class="menu-burger p-1">☰</div>
@@ -62,6 +62,6 @@
             </div>
 
         </div>
-        
+
     </div>
 </section>
